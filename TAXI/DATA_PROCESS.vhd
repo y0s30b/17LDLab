7d9aeeede7eba5bfe23a47ef8848c4732683a3ce
DESKTOP-2D1SUE6\yoseo <jinyaoxie130@naver.com>
2017-06-06 10:37:08 +0900

    SW1 input을 제외한 나머지 것들 설정

--- TAXI/DATA_PROCESS.vhd
+++ TAXI/DATA_PROCESS.vhd
@@ -13,15 +13,27 @@ entity DATA_PROCESS is
             extraCharge : out std_logic_vector(1 downto 0);
             mileageM : out std_logic_vector(12 downto 0);
             isCall : out std_logic
-            isPayment);
+            isPayment : out std_logic);
 end DATA_PROCESS;
 
 architecture DATA_Behavioral of DATA_PROCESS is
-    signal processState : std_logic_vector(1 downto 0) := "00";
+    signal processState : std_logic_vector(1 downto 0);
     -- "00": 대기, "01": "시작", "10": "정지", "11": "정산" 
-    signal SW1_flag, SW2_flag, SW3_flag : std_logic := '0';
-    signal insSW1, insSW2, insSW3 : std_logic := '0';
+    signal SW1_flag, SW2_flag, SW3_flag : std_logic;
+    signal insSW1, insSW2, insSW3 : std_logic;
+
+    signal taxiCharge_reg, taxiChargeCnt_reg : std_logic_vector(15 downto 0);
+    signal extraCharge_reg : std_logic_vector(1 downto 0);
+    signal mileageM_reg : std_logic_vector(12 downto 0);
+    signal isCall_reg, isPayment_reg : std_logic;
 begin
+    taxiCharge <= taxiCharge_reg;
+    taxiChargeCnt <= taxiChargeCnt_reg;
+    extraCharge <= extraCharge_reg;
+    mileageM <= mileageM_reg;
+    isCall <= isCall_reg;
+    isPayment <= isPayment_reg;
+
     process(SW1, SW2, SW3)
     -- switch input을 각각의 instruction signal로 바꿔주는 process ...1/2
     begin
@@ -71,5 +83,46 @@ begin
         end if;
     end process;
 
-    
+    process(RESET, CLK)
+    -- SW1~3의 신호에 따라 원하는 회로 동작을 기술하는 process
+    begin
+        if RESET = '0' then
+            processState <= "00";
+
+            SW1_flag <= '0';
+            SW2_flag <= '0';
+            SW3_flag <= '0';
+            
+            insSW1 <= '0';
+            insSW2 <= '0';
+            insSw3 <= '0';
+            
+            taxiCharge_reg <= "0000_1011_1011_1000";    -- decimal 3000
+            taxiChargeCnt_reg <= "0111_0101_0011_0000"; -- decimal 30000
+            
+            extraCharge_reg <= "00";
+            mileageM_reg <= "0000_0000_0000";
+            isCall_reg <= '0';
+            isPayment_reg <= '0';
+
+        elsif CLK = '1' and CLK'event then
+            if insSW1 = '1' then
+            end if;
+
+            if insSW2 = '1' then
+                -- 호출 여부 결정하는 신호.
+                isCall_reg <= '1';
+            end if;
+            
+            if insSW3 = '1' then
+                -- 할증 % 결정하는 신호.
+                if extraCharge_reg = "00" or extraCharge_reg = "01" then
+                    extraCharge_reg <= extraCharge_reg + 1;
+                elsif extraCharge_reg = "10" then
+                    extraCharge_reg <= "00";
+                end if;
+            end if;
+            
+        end if;
+    end process;
 end DATA_Behavioral;
